a4d2f550b9d3d9d6175cfa50049b9ac654951aae
albert <albertperezdatsira@gmail.com>
2021-09-01 18:23:07 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='init-tox',
+    author='albeertito7',
+    license='MIT'
+)
